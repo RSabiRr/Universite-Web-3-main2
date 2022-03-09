096afe4e20d75210ff42af0303a63fd410951af2
sabir <tu201806274@code.edu.az>
2022-03-09 08:54:17 +0300

    asdsad

---
+++ Universite Web/ViewModel/VmLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Universite_Web.Models;
+
+namespace Universite_Web.ViewModel
+{
+    public class VmLayout
+    {
+        public List<Slider> Sliders { get; set; }
+        public VmStudentLogin VmStudentLogin { get; set; }
+        public VmStudentRegister VmStudentRegister { get; set; }
+    }
+}
